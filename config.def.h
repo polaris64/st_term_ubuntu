f78033f9145ddad4bbe51d8afe5839e97353be16
Simon Pugnet <simon@polaris64.net>
2019-09-26 06:44:54 +0100

    Changes default config for Ubuntu with custom colour scheme

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Liberation Mono:pixelsize=12:antialias=true:autohint=true";
+static char *font = "Ubuntu Mono:pixelsize=16:antialias=true:autohint=true,Noto Color Emoji";
 static int borderpx = 2;
 
 /*
@@ -84,31 +84,27 @@ unsigned int tabspaces = 8;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
-	/* 8 normal colors */
-	"black",
-	"red3",
-	"green3",
-	"yellow3",
-	"blue2",
-	"magenta3",
-	"cyan3",
-	"gray90",
 
-	/* 8 bright colors */
-	"gray50",
-	"red",
-	"green",
-	"yellow",
-	"#5c5cff",
-	"magenta",
-	"cyan",
-	"white",
+  "#272822",
+  "#f92672",
+  "#a6e22e",
+  "#f4bf75",
+  "#66d9ef",
+  "#ae81ff",
+  "#a1efe4",
+  "#f8f8f2",
+  "#75715e",
+  "#f92672",
+  "#a6e22e",
+  "#f4bf75",
+  "#66d9ef",
+  "#ae81ff",
+  "#a1efe4",
+  "#f9f8f5",
 
 	[255] = 0,
 
 	/* more colors can be added after 255 to use with DefaultXX */
-	"#cccccc",
-	"#555555",
 };
 
 
@@ -118,8 +114,8 @@ static const char *colorname[] = {
  */
 unsigned int defaultfg = 7;
 unsigned int defaultbg = 0;
-static unsigned int defaultcs = 256;
-static unsigned int defaultrcs = 257;
+static unsigned int defaultcs = 7;
+static unsigned int defaultrcs = 0;
 
 /*
  * Default shape of cursor
